36d4a61a7f25052f048b7bd4e116ccaba2b6e904
DmitriPrijomosev <dmitri@dezajno.ee>
2022-07-04 10:17:06 +0300

    Header+2 moodili

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import background from "./img/DSC3656.jpg";
+import naturavita from "./img/N350x350.png";
+import cofesso from "./img/coffesso2.png";
+import richard from "./img/R350x350.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		  <div className='header' style={{
+			  backgroundImage: `url(${background})`,
+			  backgroundSize: 'cover',
+			  
+		  }}>
+			  	<div className='headers'>
+				  <h1 className='H1'>TANTRADE OÜ</h1>
+				  <h3 className='H2'>Kogu maailmas tunnustatud MAY-Foods tooted nüüd Eestis!</h3>
+				</div>
+		  </div>
+		  <div className='block1'>
+			  <p className='H3'>MEIE USUME, ET IDEAALNE KVALITEET SAAB OLLA SOODNE</p>
+			  <p className='text'>Just seetõttu on meil hea meel pakkuda Teile vaid parimat ettevõttelt MAY-Foods – maailma juhtivalt teeturu liidrilt.</p>
+		  </div>
+		  <div className='main-link-list'>
+			<div className='main-link'>
+				  <img src={naturavita} />
+				  <p className='text'>Naturavita</p>
+
+			  </div>
+			  <div className='main-link'>
+				  <img src={cofesso} />
+				  <p className='text'>Cofesso</p>
+
+			  </div>
+			  <div className='main-link'>
+				  <img src={richard} />
+				  <p className='text'>Richard</p>
+
+			  </div>
+		  </div>
     </div>
   );
 }
